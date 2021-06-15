6ab6a80e6e23c1eb817e92f1be3890cd69224c99
georgiosan <georgiosanagnostou@hotmail.com>
2021-06-15 19:32:02 +0300

    Added monthly payment and interest calculations

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const calculateBtn = document.querySelector(".calculate-button");
 const monthlyPayment = document.querySelector(".monthly-payment");
 const interestPaid = document.querySelector(".interest-sum");
 const principalPaid = document.querySelector(".principal-sum");
+// Form
+const calculateBtn = document.querySelector(".calculate-button");
+const loanAmount = document.querySelector(".loan-amount");
+const loanTerm = document.querySelector(".loan-term");
+const loanInterest = document.querySelector(".loan-interest");
+// Calculations
+const convertedInterest = parseFloat(loanInterest.value);
+const convertedTerm = parseFloat(loanTerm.value * 12);
+const convertedLoan = parseFloat(loanAmount.value);
 
-calculateBtn.addEventListener("click", () => {
-  calculateTotal();
+calculateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  calculateMonthlyPayment();
+  calculateTotalInterest();
 });
 
-function calculateTotal() {
-  console.log(monthlyPayment.value);
-  console.log(interestPaid.value);
-  console.log(principalPaid.value);
+function calculateMonthlyPayment() {
+  const interestToPaymentsRatio = convertedInterest / 100 / convertedTerm;
+  const actualMonthlyInterest = interestToPaymentsRatio * convertedLoan;
+  const fixedMonthlyPayment = convertedLoan / convertedTerm;
+  const actualTotal =
+    Math.round((fixedMonthlyPayment + actualMonthlyInterest) * 100) / 100;
+  monthlyPayment.innerText = `$ ${actualTotal}`;
+  monthlyPayment.style.color = "#284b63";
+}
+
+function calculateTotalInterest() {
+  const totalInterest =
+    Math.round(
+      convertedLoan * (convertedInterest / 100) * (convertedTerm / 12) * 100
+    ) / 100;
+  interestPaid.innerText = `$ ${totalInterest}`;
+  interestPaid.style.color = "#284b63";
 }
