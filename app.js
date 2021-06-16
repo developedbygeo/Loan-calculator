1ed23afa319c9de7f41517ad1fbf058776dfa356
georgiosan <georgiosanagnostou@hotmail.com>
2021-06-16 23:58:35 +0300

    Updated the calculate function

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const monthlyPayment = document.querySelector(".monthly-payment");
 const interestPaid = document.querySelector(".interest-sum");
-const principalPaid = document.querySelector(".principal-sum");
+const totalSumPaid = document.querySelector(".total-sum-paid");
 // Form
 const calculateBtn = document.querySelector(".calculate-button");
 const loanAmount = document.querySelector(".loan-amount");
 const loanTerm = document.querySelector(".loan-term");
 const loanInterest = document.querySelector(".loan-interest");
 // Calculations
-const convertedInterest = parseFloat(loanInterest.value) / 100 / 12;
-const convertedTerm = parseFloat(loanTerm.value) * 12;
-const convertedLoan = parseFloat(loanAmount.value);
 
 calculateBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -19,14 +16,16 @@ calculateBtn.addEventListener("click", (e) => {
     loanTerm.value.match(numbers) &&
     loanInterest.value.match(numbers)
   ) {
-    calculateMonthlyPayment();
-    calculateTotalInterest();
+    calculate();
   } else {
     alert("Please provide solely numeric values");
   }
 });
 
-function calculateMonthlyPayment() {
+function calculate() {
+  const convertedInterest = parseFloat(loanInterest.value) / 100 / 12;
+  const convertedTerm = parseFloat(loanTerm.value) * 12;
+  const convertedLoan = parseFloat(loanAmount.value);
   const powers = Math.pow(1 + convertedInterest, convertedTerm);
   const monthlyLoanPayment =
     (convertedLoan * powers * convertedInterest) / (powers - 1);
@@ -34,17 +33,10 @@ function calculateMonthlyPayment() {
     monthlyLoanPayment * convertedTerm -
     convertedLoan
   ).toFixed(2);
+  const totalSum = (monthlyLoanPayment * convertedTerm).toFixed(2);
   monthlyPayment.innerText = `$ ${monthlyLoanPayment.toFixed(2)}`;
   monthlyPayment.style.color = "#284b63";
-  principalPaid.innerText = `$ ${convertedLoan}`;
-  principalPaid.style.color = "#284b63";
+  totalSumPaid.innerText = `$ ${totalSum}`;
+  totalSumPaid.style.color = "#284b63";
   interestPaid.innerText = `$ ${totalInterest}`;
-}
-
-function calculateTotalInterest() {
-  const adjustedInterest = convertedInterest / 100;
-  const interestToPayments = adjustedInterest / convertedTerm;
-  const totalMonthlyInterest = interestToPayments * convertedLoan;
-  const totalAmountOfInterest =
-    Math.round(totalMonthlyInterest * convertedTerm * 100) / 100;
 }
